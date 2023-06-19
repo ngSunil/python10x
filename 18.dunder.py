8a4418b5a4cf5fc5fccb7fd2536264a1ac6eb8c1
Sunil Biswakarma <sun.aec@gmail.com>
2023-06-20 00:19:03 +0530

    add superlist example

--- 18.dunder.py
+++ 18.dunder.py
@@ -5,6 +5,30 @@ class Car:
     def __str__(self):
         return f'{self.model}'
 
+    def __len__(self):
+        return 5
+
+    def __del__(self):
+        return 'Deleted'
+
 
 tesla = Car('Tesla')
 print(tesla.__str__())
+print(tesla.__len__())
+print(tesla.__del__())
+# dunder enable instance of class or objects to interact with the built-in functions and operators of python
+
+# Example of inheritence for creating a superlist
+
+
+class SuperList(list):
+    def __len__(self):
+        return 1000
+
+
+mysuperlist = SuperList()
+print(mysuperlist.__len__())
+# see how the your SuperList class has got powers of list too
+print(mysuperlist.append(45))
+
+print(issubclass(SuperList, list))
