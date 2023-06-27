2057a823d27bdaa5998f919d7fdcc7f4de538192
Sunil Biswakarma <sun.aec@gmail.com>
2023-06-27 23:23:34 +0530

    MRO

--- 26.Inheritence_fcc.py
+++ 26.Inheritence_fcc.py
@@ -1,11 +1,17 @@
 class Fruit:
+    all = []
+
     def __init__(self, name, qty, price):
         self.name = name
         self.qty = qty
         self.price = price
+        Fruit.all.append(self)
 
     def calcTotal(self):
         return self.qty * self.price
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.name}, {self.qty}, {self.price})'
 
 
 class RottenFruit(Fruit):
@@ -21,4 +27,9 @@ fruit1 = Fruit('Mango', 3, 100)
 print(fruit1.calcTotal())
 
 rottenFruit1 = RottenFruit('Rotten Oranges', 2, 20, True)
+rottenFruit1 = RottenFruit('Rotten Apples', 2, 20, True)
+
 print(rottenFruit1.price)
+print(rottenFruit1.calcTotal())
+print(rottenFruit1)
+print(Fruit.all)
---
+++ 29.mro.py
@@ -0,0 +1,26 @@
+class X:
+    pass
+
+
+class Y:
+    pass
+
+
+class Z:
+    pass
+
+
+class A(X, Y):
+    pass
+
+
+class B(Y, Z):
+    pass
+
+
+class M(B, A, Z):
+    pass
+
+
+print(M.mro())
+# METHOD RESOLUTION ORDER
