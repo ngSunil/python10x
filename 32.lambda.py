6b52314e340da6426707397f2442924e71a7e976
Sunil Biswakarma <sun.aec@gmail.com>
2023-06-29 17:21:52 +0530

    lamda

--- 32.lambda.py
+++ 32.lambda.py
@@ -10,3 +10,24 @@ print(list(map(lambda item: item*2, my_list)))
 print(list(filter(lambda item: item % 2 == 0, [2, 5, 7, 9, 8])))
 # with reduce
 print(reduce(lambda acc, item: acc + item, my_list, 0))
+
+print(list(map(lambda item: item**2, my_list)))
+
+a = [(0, 2), (15, 1), (9, 6), (10, -1)]
+a.sort(key=lambda item: item[1])
+print(a)
+
+my_dict = [{
+    'name': 'sunil',
+    'address': 'ghy'
+},
+    {
+    'name': 'anil',
+    'address': 'titaber'
+},
+    {
+    'name': 'rita',
+    'address': 'numaligarh'
+}]
+my_dict.sort(key=lambda item: item['name'])
+print(my_dict)
