29a7698a0274ae6c8b3926473717a12ae03ccd16
Sunil Biswakarma <sun.aec@gmail.com>
2023-07-08 22:33:30 +0530

    password checker regex

---
+++ 52.regex.py
@@ -0,0 +1,15 @@
+import re
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+teststring = 'a'
+try:
+    re.fullmatch(regex, teststring).span()
+except AttributeError as err:
+    print(f'Something went wrong {err}')
+# password checker
+regex_pwd = r'[a-zA-Z0-9#$]{8,}\d$'
+try:
+    re.fullmatch(regex_pwd, input('Please enter the password: ')).span()
+    print('Strong Password')
+except AttributeError as err:
+    print(f'Not as required password')
